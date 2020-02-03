c7bd33940666ab4d813ed1410da1c82b7eba12b5
Aysen Unlu <aysenunlu@gmail.com>
2020-02-03 15:07:01 -0800

    order filtering is done-flashing problem exists

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     const row=$("#catSelect");
     const pcolumn=$("#productColumn");
+    let products=[];
     $.ajax({
         method: "GET",
         url: "/menu/getAllCategories",
@@ -19,6 +20,7 @@ $(document).ready(function() {
                 input.attr("type","checkbox");
                 input.attr("id","radio"+result[i].id);
                 input.attr("value",result[i].name);
+        
                 let label=$("<label>");
                 label.attr("class","form-check-label");
                 label.attr("for",input.attr("id"));
@@ -28,18 +30,22 @@ $(document).ready(function() {
                 div.append(div2);
                 row.append(div);
            }
-          
+           $("input[type=checkbox]").on("click", display);
            //load the products.
            $.ajax({
             method: "GET",
             url: "/menu/getAllProducts",
         
            }).done(function(reslt){
-               for(let j=0;j<reslt;j++){
+               products=reslt;
+               pcolumn.empty();
+              /* for(let j=0;j<reslt;j++){
                    //////////
+                   
                    let div=$("<div>");
                    div.attr("class","outterBox");
                    div.attr("productId","p"+reslt[i].id);
+                   div.attr("categoryId",reslt[i].categoryId);
                    let div2=$("<div>");
                    div2.attr("class","productBox");
                    let div3=$("<div>");
@@ -50,13 +56,58 @@ $(document).ready(function() {
                    div2.append(div3);
                    div.append(div2);
                    pcolumn.append(div);
+                
+                   
                    //////////
-               }
+               }*/
+              
+              
            });    
            
+       } );  
+       
+       function display(){
+           const checkBox=event.target;
+           const raw_id=event.target.id;
+           const id=raw_id.substring(5);
+           console.log(id);
+           let disp="";
 
+           if (checkBox.checked == true){
+               disp="block";
+              //text.style.display = "block";
+            } else {
+             //text.style.display = "none";
+               disp="none";
+           }
            
-       } );   
+           for (let i=0;i<products.length;i++){
+             if(products[i].categoryId==id){
+              if(disp==="block"){
+                let div=$("<div>");
+                div.attr("class","outterBox");
+                div.attr("productId","p"+products[i].id);
+                div.attr("categoryId",products[i].categoryId);
+                let div2=$("<div>");
+                div2.attr("class","productBox");
+                let div3=$("<div>");
+                div3.attr("class","productTextBox");
+                let h2=$("<h2>");
+                h2.text(products[i].name);
+                div3.append(h2);
+                div2.append(div3);
+                div.append(div2);
+                pcolumn.append(div);
+              }
+              else{
+                  $("#p"+products[i].id).remove;
+                  window.location.reload();
+              }
+            }  
+           }  
+                  
+
+       }
      
 } );   
 
